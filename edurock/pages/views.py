0bc7dd9534e02eb2274533eeedb9da0eb1400d19
sarbdeol <ubuntu@ip-172-31-25-18.eu-north-1.compute.internal>
2024-11-13 12:29:53 +0000

    all

--- edurock/pages/views.py
+++ edurock/pages/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.template import TemplateDoesNotExist
 
-@login_required
+# @login_required
 def root_page_view(request):
     try:
-        return render(request, 'pages/index.html')
+        return render(request, 'pages/maintenance.html')
     except TemplateDoesNotExist:
         return render(request, 'pages/error.html')
 
 
-@login_required
+# @login_required
 def dynamic_pages_view(request, template_name):
     try:
-        return render(request, f'pages/{template_name}.html')
+        return render(request, 'pages/maintenance.html')
+        # return render(request, f'pages/{template_name}.html')
     except TemplateDoesNotExist:
         return render(request, f'pages/error.html')
